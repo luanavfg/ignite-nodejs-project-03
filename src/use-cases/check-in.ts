922faa4c7b3e781934a541b78f64c467808c90e8
luanavfg <luanavefago1@gmail.com>
2023-10-23 10:49:45 -0300

    feat: apply rule to check-in on same day and test it

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -16,6 +16,14 @@ export class CheckInUseCase {
     gymId,
     userId,
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
+      userId,
+      new Date(),
+    )
+    if (checkInOnSameDay) {
+      throw new Error()
+    }
+
     const checkIn = await this.checkInsRepository.create({
       gym_id: gymId,
       user_id: userId,
